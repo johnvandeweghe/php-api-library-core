7677dff74c008a3a416d7e9aae78c015da8ba873
John VanDeWeghe <vandeweghe@lunixlabs.com>
2017-09-27 14:56:19 -0500

    add null cache controllers

---
+++ src/Data/CacheController/NullCacheController.php
@@ -0,0 +1,33 @@
+<?php
+namespace PHPAPILibrary\Core\Data\CacheController;
+
+use PHPAPILibrary\Core\Data\CacheControllerInterface;
+use PHPAPILibrary\Core\Data\RequestInterface;
+use PHPAPILibrary\Core\Data\ResponseInterface;
+
+/**
+ * A CacheControllerInterface implementation that does nothing. A no-cache solution.
+ * Class NullCacheController
+ * @package PHPAPILibrary\Core\Data\CacheController
+ */
+class NullCacheController implements CacheControllerInterface
+{
+
+    /**
+     * @param RequestInterface $request
+     * @return ResponseInterface|null
+     */
+    public function getResponse(RequestInterface $request)
+    {
+        return null;
+    }
+
+    /**
+     * @param RequestInterface $request
+     * @param ResponseInterface $response
+     */
+    public function storeResponse(RequestInterface $request, ResponseInterface $response): void
+    {
+        return;
+    }
+}
---
+++ src/Network/CacheController/NullCacheController.php
@@ -0,0 +1,33 @@
+<?php
+namespace PHPAPILibrary\Core\Network\CacheController;
+
+use PHPAPILibrary\Core\Network\CacheControllerInterface;
+use PHPAPILibrary\Core\Network\RequestInterface;
+use PHPAPILibrary\Core\Network\ResponseInterface;
+
+/**
+ * A CacheControllerInterface implementation that does nothing. A no-cache solution.
+ * Class NullCacheController
+ * @package PHPAPILibrary\Core\Network\CacheController
+ */
+class NullCacheController implements CacheControllerInterface
+{
+
+    /**
+     * @param RequestInterface $request
+     * @return ResponseInterface|null
+     */
+    public function getResponse(RequestInterface $request)
+    {
+        return null;
+    }
+
+    /**
+     * @param RequestInterface $request
+     * @param ResponseInterface $response
+     */
+    public function storeResponse(RequestInterface $request, ResponseInterface $response): void
+    {
+        return;
+    }
+}
