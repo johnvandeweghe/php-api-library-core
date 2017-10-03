7261fdb6c0c5ce78601e2e5b427f9daae25097dc
John VanDeWeghe <jvandeweghe@kipsu.com>
2017-10-03 17:10:52 -0500

    plan out next tests

--- tests/Data/Out/AbstractLayerControllerTest.php
+++ tests/Data/Out/AbstractLayerControllerTest.php
@@ -1,9 +1,15 @@
 <?php
 namespace PHPAPILibrary\Core\Data\Out;
 
-use PHPUnit\Framework\TestCase;
+use PHPAPILibrary\Core\Data\AccessControllerInterface;
+use PHPAPILibrary\Core\Data\CacheControllerInterface;
+use PHPAPILibrary\Core\Data\LoggerInterface;
+use PHPAPILibrary\Core\Data\RateControllerInterface;
+use PHPAPILibrary\Core\Data\ResponseInterface;
 
-class AbstractLayerControllerTest extends TestCase
+class AbstractLayerControllerTest extends \PHPAPILibrary\Core\Data\AbstractLayerControllerTest
 {
-    //public function testGetResponseReturns
+    //Test that the translators are called with request, and that when the next layer returns a response it is translated and returned
+    //Test that """                                                               """ throws an exception, it is translated to this layer's exception (with a translated response) and thrown
+    //  - For each exception type (access/rate/general)
 }
